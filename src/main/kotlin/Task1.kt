244bd803f8ed711136ddc2de766103f7ac9cc3a9
Artem Poptsov <adpoptsov@gmail.com>
2022-03-13 22:35:56 +0500

    Commit Task 1

---
+++ src/main/kotlin/Task1.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+
+    val amount = 100_00
+    val minCommission = 35_00
+    val payCommissionPercent = 75
+
+    val payCommission = amount * payCommissionPercent / 10_000
+    val finalCommission = if (payCommission >= minCommission) payCommission else minCommission
+
+    println("Сумма перевода: $amount коп.")
+    println("Размер комиссии: $finalCommission коп.")
+
+}

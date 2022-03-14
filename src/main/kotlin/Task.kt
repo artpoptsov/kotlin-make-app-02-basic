0a5ed37e5c1fa53ba7e3eeecea9e820cadf8a2a1
Artem Poptsov <adpoptsov@gmail.com>
2022-03-14 21:52:55 +0500

    Commit Task 2

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,14 +1,10 @@
 // Task #2
 fun main() {
 
-    val amount = 100_00
-    val minCommission = 35_00
-    val payCommissionPercent = 75
+    val likes = 61
 
-    val payCommission = amount * payCommissionPercent / 10_000
-    val finalCommission = if (payCommission >= minCommission) payCommission else minCommission
+    val description = if (likes * 10 - (likes / 10) * 100 == 10) "человеку" else "людям"
 
-    println("Сумма перевода: $amount коп.")
-    println("Размер комиссии: $finalCommission коп.")
+    println("Понравилось $likes $description")
 
 }

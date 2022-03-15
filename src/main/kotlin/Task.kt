0b892b1ce75e5cc6a19e97efd6cb8fbf7749995c
Artem Poptsov <adpoptsov@gmail.com>
2022-03-15 22:05:40 +0500

    Итоговый вариант

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,6 +1,22 @@
 // Task #3
 fun main() {
 
+    val lastBuy = 10_050_00
+    val discontGradeOneRub = 100_00
+    val discontGradeTwoPrecent = 5
+    val musicLover = true
+    var currentBuy = 10_320_00
 
+    if (lastBuy in 0..1000_00)  {
+        currentBuy = currentBuy
+    } else if (lastBuy in 1_001_00..10_000_00){
+        currentBuy = currentBuy - discontGradeOneRub
+    } else if (lastBuy >= 10_001_00){
+        currentBuy = (currentBuy * (100 - discontGradeTwoPrecent)) / 100
+    }
 
+    if (musicLover == true) currentBuy = currentBuy * (100 - 1)
+    val currentBuyRub = currentBuy / 10_000.0
+
+    println("Итоговая стоимость составила $currentBuyRub руб.")
 }
